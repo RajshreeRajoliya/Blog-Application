56b3474481e678c2a509c64914f90dcb154e364e
RajshreeRajoliya <rajshreerajoliya14@gmail.com>
2023-05-11 22:09:12 +0530

    final

--- client/src/components/about/About.jsx
+++ client/src/components/about/About.jsx
@@ -1,7 +1,7 @@
 
 import { Box, styled, Typography, Link } from '@mui/material';
-import { GitHub, Instagram, Email } from '@mui/icons-material';
-
+import { GitHub,  Email } from '@mui/icons-material';
+import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
 const Banner = styled(Box)`
     background-image: url(https://www.wallpapertip.com/wmimgs/23-236943_us-wallpaper-for-website.jpg);
     width: 100%;
@@ -27,23 +27,23 @@ const About = () => {
         <Box>
             <Banner/>
             <Wrapper>
-                <Typography variant="h3">Code for Interview</Typography>
+                <Typography variant="h3">Rajshree Rajoliya</Typography>
                 <Text variant="h5">I'm a Software Engineer based in India. 
                     I've built websites, desktop applications and corporate software.<br />
-                    If you are interested, you can view some of my favorite projects here
+                    If you are interested, you can view some of my favourite projects here
                     <Box component="span" style={{ marginLeft: 5 }}>
-                        <Link href="https://github.com/kunaltyagi9" color="inherit" target="_blank"><GitHub /></Link>
+                        <Link href="https://github.com/RajshreeRajoliya" color="inherit" target="_blank"><GitHub /></Link>
                     </Box>
                 </Text>
                 <Text variant="h5">
                     Need something built or simply want to have chat? Reach out to me on
                     <Box component="span" style={{ marginLeft: 5 }}>
-                        <Link href="https://www.instagram.com/codeforinterview/" color="inherit" target="_blank">
-                            <Instagram />
+                        <Link href="https://www.linkedin.com/in/rajshree-r/" color="inherit" target="_blank">
+                            <EmojiEmotionsIcon />
                         </Link>
                     </Box>  
                         or send me an Email 
-                        <Link href="mailto:codeforinterview@gmail.com?Subject=This is a subject" target="_blank" color="inherit">
+                        <Link href="mailto:rajshreerajoliya14@gmail.com?Subject=This is a subject" target="_blank" color="inherit">
                             <Email />
                         </Link>.
                 </Text>
